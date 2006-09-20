9ba1e649a7d6285fe400be9564a4d8426223ef7a
Duncan Coutts <duncan.coutts@worc.ox.ac.uk>
2006-09-21 00:31:08 +0100

    Add a bit more documentation.
    darcs-hash:1c976f01df7b6ca8d4bfbb019d2cc2bf7fd8d820

--- Codec/Compression/BZip.hs
+++ Codec/Compression/BZip.hs
@@ -19,6 +19,22 @@
 -----------------------------------------------------------------------------
 module Codec.Compression.BZip (
 
+  -- | This module provides pure functions for compressing and decompressing
+  -- streams of data represented by lazy 'ByteString's. This makes it easy to
+  -- use either in memory or with disk or network IO.
+  --
+  -- For example a simple bzip compression program is just:
+  --
+  -- > import qualified Data.ByteString.Lazy as ByteString
+  -- > import qualified Codec.Compression.BZip as BZip
+  -- >
+  -- > main = ByteString.interact BZip.compress
+  --
+  -- Or you could lazily read in and decompresse @.bz2@ file using:
+  --
+  -- > content <- fmap BZip.decompress (readFile file)
+  --
+
   -- * Compression
   compress,
   compressWith,
@@ -33,11 +49,40 @@ import Data.ByteString.Lazy (ByteString)
 
 import Codec.Compression.BZip.Internal as Internal
 
+
+-- | Decompress a stream of data in the bzip2 format.
+--
+-- There are a number of errors that can occur. In each case an exception will
+-- be thrown. The possible error conditions are:
+--
+-- * if the stream does not start with a valid gzip header
+--
+-- * if the compressed stream is corrupted
+--
+-- * if the compressed stream ends permaturely
+--
+-- Note that the decompression is performed /lazily/. Errors in the data stream
+-- may not be detected until the end of the stream is demanded (since it is
+-- only at the end that the final checksum can be checked). If this is
+-- important to you, you must make sure to consume the whole decompressed
+-- stream before doing any IO action that depends on it.
+--
 decompress :: ByteString -> ByteString
 decompress = Internal.decompressDefault
 
+
+-- | Compress a stream of data into the bzip2 format.
+--
+-- This uses the default compression level which uses the largest compression
+-- block size for the highest compression level. Use 'compressWith' to adjust
+-- the compression block size.
+--
 compress :: ByteString -> ByteString
 compress = Internal.compressDefault DefaultBlockSize
 
+
+-- | Like 'compress' but with an extra parameter to specify the block size
+-- used for compression.
+--
 compressWith :: BlockSize -> ByteString -> ByteString
 compressWith = Internal.compressDefault
